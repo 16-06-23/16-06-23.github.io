d7c44ace092449dec9a6581246f820f9ba8c4034
Alexander Bazhenov <alexbazhen@yandex.ru>
2023-03-16 00:40:51 +0400

    feat: add animation for someone blocks

--- sources/src/js/_libs_concat.cjs
+++ sources/src/js/_libs_concat.cjs
@@ -1,3 +1,8 @@
-module.exports = [ // comma separated
-    // "./src/libs/jquery/dist/jquery.min.js"
+module.exports = [
+  // comma separated
+  "./src/libs/gsap/minified/gsap.min.js",
+  "./src/libs/gsap/minified/ScrollTrigger.min.js",
+  "./src/libs/gsap/minified/SplitText.min.js",
+  "./src/libs/gsap/minified/ScrambleTextPlugin.min.js",
+  "./src/libs/gsap/minified/DrawSVGPlugin.min.js",
 ];
